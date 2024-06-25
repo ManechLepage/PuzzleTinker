dbbe57eec3d855daa5979480842f50f4f36c734b
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-06-25 17:30:16 -0400

    28: Started background system

---
+++ Puzzle Tinker/Scripts/Room/RoomBackground.gd
@@ -0,0 +1,7 @@
+class_name Background
+extends Resource
+
+@export var layer1: Texture2D
+@export var layer2: Texture2D
+@export var layer3: Texture2D
+@export var layer4: Texture2D
--- Puzzle Tinker/Scripts/Room/camera_room.gd
+++ Puzzle Tinker/Scripts/Room/camera_room.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var phantom_camera_2d = $PhantomCamera2D
+@export var background: Background
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
